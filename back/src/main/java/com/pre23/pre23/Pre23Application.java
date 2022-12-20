2d38c254ca0d09110f01db8a34e6901b60cb121a
Julymeltdown <quintuplets2000@gmail.com>
2022-12-20 15:50:00 +0900

    initial commit

---
+++ back/src/main/java/com/pre23/pre23/Pre23Application.java
@@ -0,0 +1,13 @@
+package com.pre23.pre23;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pre23Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pre23Application.class, args);
+	}
+
+}
---
+++ back/src/test/java/com/pre23/pre23/Pre23ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pre23.pre23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pre23ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
