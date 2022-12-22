5d98857b3c133fd4501227d2b9f14b9759669b71
davina708 <davinida96@gmail.com>
2022-12-22 16:59:59 +0900

    [FE]feat:styled component 초기 환경세팅

---
+++ front/src/App.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./pages/Login";
+import Logout from "./pages/Logout";
+import NotFound from "./pages/NotFound";
+import QuestionDetail from "./pages/QuestionDetail";
+import QuestionsList from "./pages/QuestionsList";
+import Signup from "./pages/Signup";
+import QuestionAsk from "./pages/QuestionAsk";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <QuestionsList />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/:questionId", //useParams()를 쓸때 ":"이용
+    element: <QuestionDetail />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/logout",
+    element: <Logout />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/register",
+    element: <QuestionAsk />,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
