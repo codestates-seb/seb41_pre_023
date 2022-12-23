f2ab28d24487b729ce34046676fafccc4066f271
nakjun12 <wns450@naver.com>
2022-12-23 14:14:16 +0900

    [FE]feat: Header, LSB 구현

--- front/src/pages/QuestionsList.jsx
+++ front/src/pages/QuestionsList.jsx
@@ -1,12 +1,21 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import LeftSidebar from "../components/LeftSidebar";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const QuestionsAll = styled.div``;
 
 export default function QuestionsList() {
   return (
     <>
       <Header />
-      <div>QuestionsList</div>
+      <Container>
+        <LeftSidebar />
+        <QuestionsAll />
+      </Container>
       <Footer />
     </>
   );
